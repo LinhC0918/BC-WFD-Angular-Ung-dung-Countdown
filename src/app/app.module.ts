d01659295bb5a5dc7aee17a5c026d6a5390a2829
LinhC0918 <nguyenvanlinh12d3@gmail.com>
2018-12-14 13:42:59 +0700

    add new file

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ColorPickerComponent } from './color-picker/color-picker.component';
+import { NameCardComponent } from './name-card/name-card.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { RatingBarComponent } from './rating-bar/rating-bar.component';
+import { CountdownTimerAliasComponent } from './countdown-timer-alias/countdown-timer-alias.component';
+import { CountdownTimerEventAliasComponent } from './countdown-timer-event-alias/countdown-timer-event-alias.component';
+import { CountdownTimerEventComponent } from './countdown-timer-event/countdown-timer-event.component';
+import { CountdownTimerGetSetComponent } from './countdown-timer-get-set/countdown-timer-get-set.component';
+import { CountdownTimerOnchangesComponent } from './countdown-timer-onchanges/countdown-timer-onchanges.component';
+import { CountdownTimerComponent } from './countdown-timer/countdown-timer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ColorPickerComponent,
+    NameCardComponent,
+    ProgressBarComponent,
+    CalculatorComponent,
+    RatingBarComponent,
+    CountdownTimerAliasComponent,
+    CountdownTimerEventAliasComponent,
+    CountdownTimerEventComponent,
+    CountdownTimerGetSetComponent,
+    CountdownTimerOnchangesComponent,
+    CountdownTimerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
